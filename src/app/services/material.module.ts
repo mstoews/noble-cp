4b3da92241a65db737dc3a7288000201c676d6b2
mstoews <mstoews@hotmail.com>
2024-05-24 09:53:21 -0400

    update ng 18

--- src/app/services/material.module.ts
+++ src/app/services/material.module.ts
@@ -43,7 +43,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatTreeModule} from '@angular/material/tree';
 import {OverlayModule} from '@angular/cdk/overlay';
-import {HttpClientModule } from '@angular/common/http'
+import {} from '@angular/common/http'
 import {FormsModule } from '@angular/forms';
 import {ReactiveFormsModule } from '@angular/forms';
 
